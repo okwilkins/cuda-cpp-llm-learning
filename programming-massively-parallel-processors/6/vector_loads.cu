8fe8f01597acbfd3c3c26b88b5c3d82caf186f80
Oliver Wilkins <okwilkins@googlemail.com>
2026-06-12 21:43:18 +0100

    feat(ppmp/6): Coarsened vector add

--- programming-massively-parallel-processors/6/vector_loads.cu
+++ programming-massively-parallel-processors/6/vector_loads.cu
@@ -36,6 +36,21 @@ __global__ void vectorLoadsVectorAdd(float *const __restrict__ A, float *const _
     ((float4 *)out)[idx] = out4;
 }
 
+__global__ void coarsenedVectorAdd(float *const __restrict__ A, float *const __restrict__ B,
+                                   float *const __restrict__ out, unsigned int size,
+                                   int coarseningFactor) {
+    const unsigned int idx{(blockDim.x * blockIdx.x + threadIdx.x) * coarseningFactor};
+
+    for (int i{}; i < coarseningFactor; ++i) {
+        const unsigned int coarsenedIdx{idx + i};
+        if (coarsenedIdx >= size) {
+            return;
+        }
+
+        out[coarsenedIdx] = A[coarsenedIdx] + B[coarsenedIdx];
+    }
+}
+
 int main() {
     cudaDeviceProp prop{};
     CUDA_CHECK(cudaGetDeviceProperties(&prop, 0));
@@ -64,10 +79,17 @@ int main() {
     std::cout << "========================\n\n";
 
     constexpr unsigned int vecSize{2048 * 128};
+    constexpr int coarseningFactor{32};
+
     constexpr unsigned int vec4Count{vecSize / 4};
+    constexpr unsigned int coarsenedCount{vecSize / coarseningFactor};
+
     const int threadsPerBlock{prop.maxThreadsPerMultiProcessor / 4};
+
     const int blocks{(static_cast<int>(vecSize) + threadsPerBlock - 1) / threadsPerBlock};
     const int blocksVec4{(static_cast<int>(vec4Count) + threadsPerBlock - 1) / threadsPerBlock};
+    const int blocksCoarsened{(static_cast<int>(coarsenedCount) + threadsPerBlock - 1) /
+                              threadsPerBlock};
 
     DefaultVector<vecSize> A{};
     DefaultVector<vecSize> B{};
@@ -110,6 +132,21 @@ int main() {
 
     vectorLoadsVectorAdd<<<blocksVec4, threadsPerBlock>>>(A.devicePtr, B.devicePtr, out.devicePtr,
                                                           vec4Count);
+
+    cudaDeviceSynchronize();
+    CUDA_CHECK(cudaMemcpy(out.data.data(), out.devicePtr, out.memSize, cudaMemcpyDeviceToHost));
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
+    out = DefaultVector<vecSize>{};
+
+    std::cout << "\n\n========================\n";
+    std::cout << "Launching coarsened vector add kernel with:\n";
+    std::cout << "\tNum blocks           : " << blocksCoarsened << '\n';
+    std::cout << "\tNum threads per block: " << threadsPerBlock << '\n';
+    std::cout << "========================\n\n";
+
+    coarsenedVectorAdd<<<blocksCoarsened, threadsPerBlock>>>(
+        A.devicePtr, B.devicePtr, out.devicePtr, vecSize, coarseningFactor);
 
     cudaDeviceSynchronize();
     CUDA_CHECK(cudaMemcpy(out.data.data(), out.devicePtr, out.memSize, cudaMemcpyDeviceToHost));
